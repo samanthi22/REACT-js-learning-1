efd7e1ca29591f83e03eca7ae73dc67f0e556dea
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-10-18 18:06:10 +0000

    Map function

---
+++ g.js
@@ -0,0 +1,18 @@
+function map(arr, fn) {
+    const newArr = []
+    
+    // every value in array runs it through a function and returns another array with return values
+    for (let i = 0; i < arr.length; i++) {
+        let val = arr[i]
+        newArr.push(fn(val))
+    }
+    
+    return newArr
+}
+
+function addOne(num) { return num + 1 }
+
+const x = [0,1,2,3]
+
+console.log(map(x, addOne))
+// [1,2,3,4]
