552657915d12ddd7b4c10524442deaebfe76dd80
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-10-18 17:48:28 +0000

    filter method

--- week2/e.js
+++ week2/e.js
@@ -5,3 +5,14 @@ console.log(addOne(1))
 
 // map function to each value returns array 
 console.log(x.map(addOne))
+
+// filter
+
+function isGreaterThanOne(num) { returns num > 1 }
+
+console.log(isGreaterThanOne(100))
+console.log(isGreaterThanOne(1))
+
+// filter is greater than
+// then 2,3 we get back
+console.log(x.filter(isGreaterThanOne))
