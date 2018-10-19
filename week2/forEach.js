a6645bb37a8e0652fc5c5039709babaefe73dd2a
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-10-19 10:36:30 +0000

    setTimeout

---
+++ week2/forEach.js
@@ -0,0 +1,44 @@
+function map(arr, fn) {
+    const newArr = []
+    
+    // every value in array runs it through a function and returns another array with return values
+    arr.forEach(function(val) {
+        
+        newArr.push(fn(val))
+    })
+    
+    return newArr
+}
+
+function addOne(num) { return num + 1 }
+
+const x = [0,1,2,3]
+
+console.log(map(x, addOne))
+// [1,2,3,4]
+
+//synchronous, async, single-threaded
+function hang(secs) {
+    const doneAt = Date.now() + (secs * 1000)
+    while (Date.now() < doneAt) {}
+}
+
+// a function that takes a long time will cause a page to become unresponsive
+hang(10)
+
+function printOne() {
+    console.log('one')
+}
+
+
+function printTwo() {
+    console.log('two')
+}
+
+function printThree() {
+    console.log('three')
+}
+
+setTimeout(printOne, 1000)
+setTimeout(printTwo, 0)
+printThree()
